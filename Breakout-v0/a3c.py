db65850351e065fb34371f2aeac6104c2c103867
7Aces <sanyam.satia@ymail.com>
2017-04-10 22:58:04 -0500

    Add A3C code for Breakout-v0

--- Breakout-v0/a3c.py
+++ Breakout-v0/a3c.py
@@ -1,4 +1,5 @@
 import gym
+from atari_environment import AtariEnvironment
 import multiprocessing
 import numpy as np
 import tensorflow as tf
@@ -9,7 +10,7 @@ from time import sleep
 # Look into cost fn constants
 
 class AC_Network():
-    def __init__(self, state_size, action_size, scope, optimizer):
+    def __init__(self, action_size, scope, optimizer):
         with tf.variable_scope(scope):
             HIDDEN_LAYER_1_SIZE = 8
             HIDDEN_LAYER_2_SIZE = 8
@@ -17,25 +18,29 @@ class AC_Network():
             W2 = 1.0
             W3 = 1e-2
 
-            self.input_layer = tf.placeholder(shape = [None, state_size], dtype = tf.float32)
-            self.hidden_layer_1 = slim.fully_connected(self.input_layer, HIDDEN_LAYER_1_SIZE, biases_initializer = None, activation_fn = tf.nn.relu)
-            # self.hidden_layer_2 = slim.fully_connected(self.hidden_layer_1, HIDDEN_LAYER_2_SIZE, biases_initializer = None, activation_fn = tf.nn.elu)
+            self.input_layer = tf.placeholder(shape = [None, 4, 84, 84], dtype = tf.float32)
+            self.normalized_input = tf.div(tf.to_float(self.input_layer), 255.0)
 
-            self.policy_layer = slim.fully_connected(self.hidden_layer_1, action_size, biases_initializer = None, activation_fn = tf.nn.softmax)
-            self.value_layer = slim.fully_connected(self.hidden_layer_1, 1, biases_initializer = None, activation_fn = None)
+            self.convolution_layer_1 = slim.conv2d(self.input_layer, 16, 8, 4)
+            self.convolution_layer_2 = slim.conv2d(self.convolution_layer_1, 32, 4, 4)
+            self.fully_connected_layer_1 = slim.fully_connected(slim.flatten(self.convolution_layer_2), 256, biases_initializer = None, activation_fn = tf.identity)
+
+            self.policy_layer = slim.fully_connected(self.fully_connected_layer_1, action_size, biases_initializer = None, activation_fn = tf.nn.softmax)
+            self.value_layer = slim.fully_connected(self.fully_connected_layer_1, 1, biases_initializer = None, activation_fn = None)
 
             if scope != 'global':
                 self.actions = tf.placeholder(shape = [None], dtype = tf.int32)
                 self.actions_onehot = tf.one_hot(self.actions, action_size, dtype=tf.float32)
-                self.target_value = tf.placeholder(shape = [None], dtype = tf.float32)
+                self.target_values = tf.placeholder(shape = [None], dtype = tf.float32)
                 self.advantages = tf.placeholder(shape = [None], dtype = tf.float32)
-                self.responsible_outputs = tf.reduce_sum(self.policy_layer * self.actions_onehot, [1])
 
-                self.value_function_loss = tf.reduce_sum(tf.square(self.target_value - tf.reshape(self.value_layer, [-1])))
+                # self.value_function_loss = tf.reduce_sum(tf.square(self.target_values - tf.reshape(self.value_layer, [-1])))
+                self.value_function_loss = tf.reduce_mean(tf.square(tf.subtract(self.target_values, self.value_layer)))
                 # self.policy_loss = - tf.reduce_sum(tf.multiply(tf.transpose(tf.square(self.actions)), self.advantages))
-                self.policy_loss = - tf.reduce_sum(tf.multiply(tf.log(self.responsible_outputs), self.advantages))
-                self.entropy = - tf.reduce_sum(tf.multiply(self.policy_layer, tf.log(self.policy_layer)))
-                self.total_loss = W1 * self.value_function_loss + W2 * self.policy_loss + W3 * self.entropy
+                self.policy_loss = - tf.reduce_sum(tf.multiply(tf.log(self.policy_layer), self.actions_onehot), reduction_indices = 1)
+                self.policy_loss = tf.multiply(self.policy_loss, tf.subtract(self.target_values, self.value_layer))
+                # self.entropy = - tf.reduce_sum(tf.multiply(self.policy_layer, tf.log(self.policy_layer)))
+                self.total_loss = W1 * self.value_function_loss + W2 * self.policy_loss #+ W3 * self.entropy
 
                 train_vars_local = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
                 self.gradients = tf.gradients(self.total_loss, train_vars_local)
@@ -47,11 +52,15 @@ class AC_Network():
 class Worker():
     GAMMA = 0.99
 
-    def __init__(self, number, env, state_size, action_size, optimizer):
+    def __init__(self, number, env, action_size, optimizer):
+        RESIZED_WIDTH = 84
+        RESIZED_HEIGHT = 84
+        AGENT_HISTORY_LENGTH = 4
+
         self.name = 'worker_' + str(number)
         self.optimizer = optimizer
-        self.local_AC_Network = AC_Network(state_size, action_size, self.name, self.optimizer)
-        self.env = env
+        self.local_AC_Network = AC_Network(action_size, self.name, self.optimizer)
+        self.env = AtariEnvironment(gym_env = env, resized_width = RESIZED_WIDTH, resized_height = RESIZED_HEIGHT, agent_history_length = AGENT_HISTORY_LENGTH)
 
         global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
         local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.name)
@@ -83,9 +92,9 @@ class Worker():
         advantages = self.discount_rewards(advantages)
 
         feed_dict = {
-            self.local_AC_Network.input_layer: np.vstack(states),
+            self.local_AC_Network.input_layer: states.tolist(),
             self.local_AC_Network.actions: actions,
-            self.local_AC_Network.target_value: discounted_rewards,
+            self.local_AC_Network.target_values: discounted_rewards,
             self.local_AC_Network.advantages: advantages
         }
 
@@ -103,21 +112,10 @@ class Worker():
                 sess.run(self.update_local_network)
                 episode_history = []
                 episode_reward = 0
-                state = self.env.reset()
+                state = self.env.get_initial_state()
                 done = False
 
                 while not done:
-                    if len(episode_history) >= EPISODE_BUFFER_SIZE:
-                        value = sess.run(
-                                    self.local_AC_Network.value_layer,
-                                    feed_dict = {
-                                        self.local_AC_Network.input_layer: [state]
-                                    }
-                                )
-                        self.train(sess, episode_history, value[0][0])
-                        sess.run(self.update_local_network)
-                        episode_history = []
-
                     action_dist, value = sess.run([
                         self.local_AC_Network.policy_layer,
                         self.local_AC_Network.value_layer
@@ -126,11 +124,12 @@ class Worker():
                         self.local_AC_Network.input_layer: [state]
                     })
 
-                    # actions = np.squeeze(actions)
-                    action = np.random.choice(action_dist[0], p = action_dist[0])
-                    action = np.argmax(action_dist == action)
+                    action_dist = np.squeeze(action_dist)
+                    # print action_dist
+                    action = np.random.choice(range(len(action_dist)), p = action_dist)
+                    value = np.squeeze(value)
                     next_state, reward, done, _ = self.env.step(action)
-                    episode_history.append([state, action, reward, next_state, value[0, 0]])
+                    episode_history.append([state.tolist(), action, reward, next_state.tolist(), value])
                     episode_reward += reward
                     state = next_state
 
@@ -143,9 +142,8 @@ class Worker():
 
 
 if __name__ == '__main__':
-    STATE_SIZE = 8
-    ACTION_SIZE = 4
-    ALPHA = 1e-3
+    ACTION_SIZE = 3
+    ALPHA = 1e-4
 
     MAX_EPISODES = 10000
 
@@ -153,13 +151,13 @@ if __name__ == '__main__':
 
     with tf.device("/cpu:0"):
         optimizer = tf.train.AdamOptimizer(learning_rate = ALPHA)
-        master_network = AC_Network(STATE_SIZE, ACTION_SIZE, 'global', optimizer)
+        master_network = AC_Network(ACTION_SIZE, 'global', optimizer)
         num_workers = multiprocessing.cpu_count()
 
         workers = []
         for i in range(num_workers):
-            env = gym.make('LunarLander-v2')
-            new_worker = Worker(i, env, STATE_SIZE, ACTION_SIZE, optimizer)
+            env = gym.make('Breakout-v0')
+            new_worker = Worker(i, env, ACTION_SIZE, optimizer)
             workers.append(new_worker)
 
     with tf.Session() as sess:
